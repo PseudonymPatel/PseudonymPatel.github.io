2da2b5e4ed1b6fc6c9aa587307b0d39c7966fce1
PseudonymPatel <programmingpatel@gmail.com>
2020-10-25 09:28:02 -0400

    site-wide fixes, new blog post

---
+++ js/blogjs.js
@@ -0,0 +1,18 @@
+// This is for the little bit of js in the blog post (collapsibles)
+
+window.addEventListener("load", function() {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+});
